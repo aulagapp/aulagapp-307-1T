b14e8dea8709ea31b6221468e472610d11e67bb8
aulagapp <aulagapp@calpoly.edu>
2018-10-05 15:01:25 -0700

    commit3

---
+++ src/module.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+/**
+ * @author arun2
+ *
+ */
+public class module{
+public static void main(String[] args){
+    int a = 3;
+    int b = 4;
+    int c = a + b;
+    System.out.println(c);
+}
+}
